fd709436f45506ae1201d0646058e3034490c6c6
medinag-codes <medinageyer@gmail.com>
2025-03-09 09:02:54 -0400

    My solution

---
+++ 7kyu/Friend or Foe?.js
@@ -0,0 +1,26 @@
+// href = "https://www.codewars.com/kata/55b42574ff091733d900002f"
+
+// Description:
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Input = ["Ryan", "Kieran", "Jason", "Yous"]
+// Output = ["Ryan", "Yous"]
+
+// Input = ["Peter", "Stephen", "Joe"]
+// Output = []
+// Input strings will only contain letters.
+// Note: keep the original order of the names in the output.
+
+function friend(friends){
+    let friendList = []
+    for(let i = 0; i < friends.length; i++){
+      if(friends[i].length === 4){
+        friendList.push(friends[i])
+      }else{
+      
+      }
+    }
+    return(friendList)
+  }
